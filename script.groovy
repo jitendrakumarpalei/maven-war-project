bc24cf82322e8e880c015ee924b7ffd0b60c3091
Chinmaya Pradhan <118071349+chinmaya10000@users.noreply.github.com>
2023-05-31 18:09:41 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image"
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t chinmayapradhan/tomcat-app:lts .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push chinmayapradhan/tomcat-app:lts'
